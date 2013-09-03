eee57ceaf71a670875efe472703e9e70280ad0aa
Loïc Frering <loic.frering@gmail.com>
2013-09-03 17:16:03 +0200

    Make the user informations configurable via user variables.

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Set up sudo
-echo %loic ALL=NOPASSWD:ALL > /etc/sudoers.d/loic
-chmod 0440 /etc/sudoers.d/loic
+echo %$SUDO_USER ALL=NOPASSWD:ALL > /etc/sudoers.d/$SUDO_USER
+chmod 0440 /etc/sudoers.d/$SUDO_USER
 
 # Set up sudo to allow no-password sudo for "sudo"
-usermod -a -G sudo loic
+usermod -a -G sudo $SUDO_USER
 
 # Installing vagrant keys
-mkdir /home/loic/.ssh
-chmod 700 /home/loic/.ssh
-cd /home/loic/.ssh
+mkdir /home/$SUDO_USER/.ssh
+chmod 700 /home/$SUDO_USER/.ssh
+cd /home/$SUDO_USER/.ssh
 wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
-chmod 600 /home/loic/.ssh/authorized_keys
-chown -R loic /home/loic/.ssh
+chmod 600 /home/$SUDO_USER/.ssh/authorized_keys
+chown -R $SUDO_USER /home/$SUDO_USER/.ssh
 
 # Install NFS for Vagrant
 aptitude update
